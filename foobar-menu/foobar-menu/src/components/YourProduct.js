afe35b0d6b7c24d7f172495ebdea9f664241630d
Agata Figura <agata.figura1@gmail.com>
2021-12-03 15:29:30 +0100

    popup

--- foobar-menu/foobar-menu/src/components/YourProduct.js
+++ foobar-menu/foobar-menu/src/components/YourProduct.js
@@ -3,10 +3,11 @@ import bottle from "../img/bottles/elhefe.png";
 import close from '../img/icons/close.svg';
 
 export default function YourProduct() {
+  
     return(
     <div className="your-product">
       <div className="product-details">
-          <img className="bottle" src={bottle}></img>
+          <img className="bottle" src={bottle} alt="beer bottle"></img>
           <div className="details">
             <h3>El Hefe</h3>
             <p>60 dkk</p>
